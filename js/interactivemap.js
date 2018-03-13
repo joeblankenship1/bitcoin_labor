832b39d5aaf9bd936ce1815c238cc71dd350645b
joeblankenship1 <joe.blankenship1@gmail.com>
2018-03-13 18:15:54 -0400

    added ui control for layers, modified lorem text, adjust output section styling

--- js/interactivemap.js
+++ js/interactivemap.js
@@ -18,99 +18,135 @@
         opacity: 0.5
     }).addTo(map);
 
-    var cables = $.getJSON("data/cables.json", function(data) {
-        L.geoJSON(data, {
-            style: function(feature) {
-                return {
-                    color: 'yellow',
-                    weight: 0.5,
-                    opacity: 0.65
-                };
-            }
-        }).addTo(map);
-    });
+    drawMap();
 
-    var landingPoints = $.getJSON("data/landingPoints.json", function(data) {
+    function drawMap() {
 
-        var geojsonMarkerOptions = {
-            radius: 3,
-            fillColor: "#73b553",
-            color: "gray",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        };
-
-        L.geoJSON(data, {
-            pointToLayer: function(feature, latlng) {
-                return L.circleMarker(latlng, geojsonMarkerOptions)
-            }
-        }).addTo(map);
-    });
-
-    var landingPoint_density = $.getJSON("data/landingPoints.json", function(data) {
-        var locations = data.features.map(function(nodes) {
-            var location = nodes.geometry.coordinates.reverse();
-            location.push(0.5);
-            return location;
+        var cables = $.getJSON("data/cables.json", function(data) {
+            return data;
         });
 
-        var heat = L.heatLayer(locations, {radius: 25, gradient: {1: 'red'}, blur: 0});
-        map.addLayer(heat);
-
-    });
-
-    var bitnodes_data = $.getJSON("data/bitnodes.json", function(data) {
-
-        var geojsonMarkerOptions = {
-            radius: 2,
-            fillColor: "#ffcd37",
-            color: "black",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-        };
-
-        L.geoJSON(data, {
-            pointToLayer: function(feature, latlng) {
-                return L.circleMarker(latlng, geojsonMarkerOptions)
-            }
-        }).addTo(map);
-    });
-
-    var bitnodes_density = $.getJSON("data/bitnodes.json", function(data) {
-        var locations = data.features.map(function(nodes) {
-            var location = nodes.geometry.coordinates.reverse();
-            location.push(0.5);
-            return location;
+        var landingPoints = $.getJSON("data/landingPoints.json", function(data) {
+            return data;
         });
 
-        var heat = L.heatLayer(locations, {radius: 25, gradient: {1: 'blue'}, blur: 0});
-        map.addLayer(heat);
+        var bitnodes_data = $.getJSON("data/bitnodes.json", function(data) {
+            return data;
+        });
 
-    });
+        map.fitBounds([
+            [90, -180],
+            [-90, 180]
+        ]);
 
-    /*
-    var baseMaps = {
-        "Stamen": tiles
-    };
+        map.setZoom(map.getZoom() - .2);
 
-    var overlayMaps = {
-        "Bitnodes": bitnodes,
-        "Cables": cables,
-        "Landing Points": landingPoints,
-        "Node Density": bitnodes_density,
-        "Landing Points Density": landingPoint_density
-    };
+        createCheckboxUI(cables, landingPoints, bitnodes_data);
 
+    }
 
-    L.control.layers(baseMaps, overlayMaps).addTo(map);
-    */
-    map.fitBounds([
-        [90, -180],
-        [-90, 180]
-    ]);
+    function createCheckboxUI(cables, landingPoints, bitnodes_data) {
+        // create Leaflet control for slider
+        var checkboxControl = L.control({
+            position: 'bottomleft'
+        });
+        // define the ui-control within the DOM
+        checkboxControl.onAdd = function(map) {
+            // use the defined ui-control element
+            var checkbox = L.DomUtil.get("ui-controls");
+            // diable click events
+            L.DomEvent.disableClickPropagation(checkbox);
+            return checkbox;
+        };
+        // add control to map
+        checkboxControl.addTo(map);
 
-    map.setZoom(map.getZoom() - .2);
+        $("input[type='checkbox']").on('input change', function() {
+            // event defined as currentYear
+            var currentLayer = this.name;
+            // update the map year
+            updateMap(cables, landingPoints, bitnodes_data, currentLayer)
+        });
+
+    }
+
+    function updateMap(cables, landingPoints, bitnodes_data, currentLayer) {
+
+        if (currentLayer == cables) {
+            var dataLayer = L.geoJSON(cables, {
+                style: function(feature) {
+                    return {
+                        color: 'yellow',
+                        weight: 0.5,
+                        opacity: 0.65
+                    };
+                }
+            }).addTo(map);
+        }
+        else if (currentLayer == landingPoints) {
+            var geojsonMarkerOptions = {
+                radius: 3,
+                fillColor: "#73b553",
+                color: "gray",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            var dataLayer = L.geoJSON(landingPoints, {
+                pointToLayer: function(feature, latlng) {
+                    return L.circleMarker(latlng, geojsonMarkerOptions)
+                }
+            }).addTo(map);
+        }
+        else if (currentLayer == bitnodes_data) {
+            var geojsonMarkerOptions = {
+                radius: 2,
+                fillColor: "#ffcd37",
+                color: "black",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            var dataLayer = L.geoJSON(bitnodes_data, {
+                pointToLayer: function(feature, latlng) {
+                    return L.circleMarker(latlng, geojsonMarkerOptions)
+                }
+            }).addTo(map);
+        }
+        else if (currentLayer == 'bitnodes_density') {
+            var locations = bitnodes_data.features.map(function(nodes) {
+                var location = nodes.geometry.coordinates.reverse();
+                location.push(0.5);
+                return location;
+            });
+
+            var heat = L.heatLayer(locations, {
+                radius: 25,
+                gradient: {
+                    1: 'blue'
+                },
+                blur: 0
+            });
+            map.addLayer(heat);
+        }
+        else if (currentLayer == 'landingPoint_density') {
+            var locations = landingPoints.features.map(function(points) {
+                var location = points.geometry.coordinates.reverse();
+                location.push(0.5);
+                return location;
+            });
+
+            var heat = L.heatLayer(locations, {
+                radius: 25,
+                gradient: {
+                    1: 'red'
+                },
+                blur: 0
+            });
+            map.addLayer(heat);
+        }
+    }
 
 })();
